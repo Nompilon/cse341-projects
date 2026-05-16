575c743b47e20f5e76aef8841bfdc48123c07953
Nompilon <ngwenyanompilo@gmail.com>
2026-05-16 22:08:12 +0200

    week 1 done

---
+++ contacts-api/controllers/contacts.js
@@ -0,0 +1,48 @@
+const { ObjectId } = require('mongodb');
+const { getDB } = require('../db');
+
+const getAllContacts = async (req, res) => {
+  try {
+    const db = getDB();
+
+    const contacts = await db
+      .collection('contacts')
+      .find()
+      .toArray();
+
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+const getSingleContact = async (req, res) => {
+  try {
+    const db = getDB();
+
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = await db
+      .collection('contacts')
+      .findOne({ _id: contactId });
+
+    if (!contact) {
+      return res.status(404).json({
+        message: 'Contact not found'
+      });
+    }
+
+    res.status(200).json(contact);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getSingleContact
+};
