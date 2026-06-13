dc426b29ce45fa72f46c153d8b958cdd3a747db0
Nompilon <ngwenyanompilo@gmail.com>
2026-06-13 21:36:39 +0200

    ...

--- TikCat-API/src/config/db.js
+++ TikCat-API/src/config/db.js
@@ -4,13 +4,14 @@ const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
 
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "TikCatDB"
+    });
 
     console.log("MongoDB Connected");
     console.log("Database:", mongoose.connection.name);
   } catch (error) {
     console.error("Database connection failed:", error.message);
-
     process.exit(1);
   }
 };
